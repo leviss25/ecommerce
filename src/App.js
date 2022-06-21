ac9c3718af49a5458f5d1d0848d49268a6e6cbde
Carl Leviss <clmayhuap@gmail.com>
2022-06-20 22:17:53 -0500

    creamos el navbar y su css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Navbar/>
     </div>
   );
 }
